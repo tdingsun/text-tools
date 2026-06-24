3b46246c82f15e69f5c1b187a55c1b33ca1c375f
tdingsun <tiger.dingsun@gmail.com>
2026-06-24 14:50:14 -0700

    highlight current nav link

--- src/lib/components/NavLink.svelte
+++ src/lib/components/NavLink.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+import { page } from "$app/state";
 	let { href, children } = $props();
 </script>
 
-<a {href} class="border px-2 py-1 rounded hover:bg-green-50 cursor-pointer">
+<a {href} class="border px-2 py-1 rounded hover:bg-green-50 {page.url.pathname === href ? 'bg-green-200' : ''} cursor-pointer">
 	{@render children?.()}
 </a>
